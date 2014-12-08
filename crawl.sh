dfa82fe670d66cbd1508f2259e0aea66c3521dd1
jonemerson <jon@jonemerson.net>
2014-12-07 16:45:26 -0800

    Move build process to Maven, since it's a pain to deploy to Heroku with Ant.  (I couldn't get dennisg/heroku-buildpack-ant to work: Ant couldn't find a Java runtime, despite it being set in the Heroku environment variables.)

--- crawl.sh
+++ crawl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ant
+mvn package
 if [ $? -eq 0 ]; then
-    java -cp target/classes:"dist/lib/twitternews.jar:lib/*" \
+    java -cp target/classes:"target/NewsServer-1.0-SNAPSHOT.jar:target/dependency/*" \
             -Xdebug \
             -Xrunjdwp:transport=dt_socket,address=8008,server=y,suspend=n \
             -Dcom.sun.media.imageio.disableCodecLib=true \
