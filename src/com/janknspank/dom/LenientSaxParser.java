c04c46342fced5e0aa9ce67d68ccdef4e7ea2f68
jonemerson <jon@jonemerson.net>
2014-12-08 22:19:16 -0800

    Create a very lenient HTML parser that follows the pattern/API of Java's SAX parsers.  This guy won't barf on "malformed" XML that exists on about 50% of NYTimes articles and much more of other sites'.

--- src/com/janknspank/dom/LenientSaxParser.java
+++ src/com/janknspank/dom/LenientSaxParser.java
@@ -1,9 +1,16 @@
 package com.janknspank.dom;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import javax.xml.parsers.SAXParser;
 
+import org.xml.sax.Attributes;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.DTDHandler;
 import org.xml.sax.EntityResolver;
@@ -14,9 +21,343 @@ import org.xml.sax.SAXException;
 import org.xml.sax.SAXNotRecognizedException;
 import org.xml.sax.SAXNotSupportedException;
 import org.xml.sax.XMLReader;
+import org.xml.sax.ext.Attributes2Impl;
 
+/**
+ * A class that implements Java's SAXParser pattern in a way that doesn't fail
+ * due to malformed HTML/XML.
+ */
 @SuppressWarnings("deprecation")
 public class LenientSaxParser extends SAXParser {
+  private final LenientXMLReader xmlReader;
+
+  /**
+   * Parses an element, e.g. <div> or <div foo="true">, into its tag name and
+   * attributes.
+   */
+  static class LenientElementInterpreter {
+    private String tag = null;
+    private boolean selfClosing = false;
+    private Attributes2Impl attributes = new Attributes2Impl();
+
+    private enum InterpreterState {
+      TAG,
+      ENIGMATIC_VOID,
+      ATTRIBUTE_NAME,
+      ATTRIBUTE_VALUE,
+      ATTRIBUTE_VALUE_INSIDE_SINGLE_QUOTE,
+      ATTRIBUTE_VALUE_INSIDE_DOUBLE_QUOTE;
+    }
+
+    public LenientElementInterpreter(String element) {
+      if (element.charAt(0) != '<') {
+        throw new IllegalStateException(
+            "Elements must start with <.  Instead received: " + element);
+      }
+      InterpreterState state = InterpreterState.TAG;
+      selfClosing = element.endsWith("/>");
+      StringBuilder b = new StringBuilder();
+      String currentAttributeName = null;
+      for (int i = 1; i < element.length(); i++) {
+        char c = element.charAt(i);
+        if (Character.isWhitespace(c) || i == element.length() - (selfClosing ? 2 : 1)) {
+          switch (state) {
+            case TAG:
+              tag = b.toString();
+              b.setLength(0);
+              state = InterpreterState.ENIGMATIC_VOID;
+              break;
+            case ATTRIBUTE_NAME:
+              String attributeName = b.toString();
+              attributes.addAttribute(
+                  /* uri */ "",
+                  /* localName */ "",
+                  /* qName */ attributeName,
+                  /* type */ "",
+                  /* value */ attributeName);
+              b.setLength(0);
+              state = InterpreterState.ENIGMATIC_VOID;
+              break;
+            case ATTRIBUTE_VALUE:
+              // We haven't seen a quote yet... So let's end the value...
+              attributes.addAttribute(
+                  /* uri */ "",
+                  /* localName */ "",
+                  /* qName */ currentAttributeName,
+                  /* type */ "",
+                  /* value */ b.toString());
+              currentAttributeName = null;
+              b.setLength(0);
+              state = InterpreterState.ENIGMATIC_VOID;
+              break;
+            case ENIGMATIC_VOID:
+              break;
+            default:
+              // These are spaces within quoted attribute values.
+              b.append(c);
+          }
+        } else if (c == '=' && state == InterpreterState.ATTRIBUTE_NAME) {
+          currentAttributeName = b.toString();
+          b.setLength(0);
+          state = InterpreterState.ATTRIBUTE_VALUE;
+        } else if (c == '"' && b.length() == 0 && state == InterpreterState.ATTRIBUTE_VALUE) {
+          state = InterpreterState.ATTRIBUTE_VALUE_INSIDE_DOUBLE_QUOTE;
+        } else if (c == '"' && state == InterpreterState.ATTRIBUTE_VALUE_INSIDE_DOUBLE_QUOTE) {
+          attributes.addAttribute(
+              /* uri */ "",
+              /* localName */ "",
+              /* qName */ currentAttributeName,
+              /* type */ "",
+              /* value */ b.toString());
+          currentAttributeName = null;
+          b.setLength(0);
+          state = InterpreterState.ENIGMATIC_VOID;
+        } else if (c == '\'' && b.length() == 0 && state == InterpreterState.ATTRIBUTE_VALUE) {
+          state = InterpreterState.ATTRIBUTE_VALUE_INSIDE_SINGLE_QUOTE;
+        } else if (c == '\'' && state == InterpreterState.ATTRIBUTE_VALUE_INSIDE_SINGLE_QUOTE) {
+          attributes.addAttribute(
+              /* uri */ "",
+              /* localName */ "",
+              /* qName */ currentAttributeName,
+              /* type */ "",
+              /* value */ b.toString());
+          currentAttributeName = null;
+          b.setLength(0);
+          state = InterpreterState.ENIGMATIC_VOID;
+        } else if ((c == '\'' || c == '"') && state == InterpreterState.ATTRIBUTE_NAME) {
+          // This is illegal: There shouldn't be quotes inside attribute names.
+          // To handle this "best" let's just commit the attribute name so far
+          // and ignore the quote.
+          String attributeName = b.toString();
+          attributes.addAttribute(
+              /* uri */ "",
+              /* localName */ "",
+              /* qName */ attributeName,
+              /* type */ "",
+              /* value */ attributeName);
+          b.setLength(0);
+          state = InterpreterState.ENIGMATIC_VOID;
+        } else {
+          if (state == InterpreterState.ENIGMATIC_VOID) {
+            state = InterpreterState.ATTRIBUTE_NAME;
+          }
+          b.append(c);
+        }
+      }
+    }
+
+    public String getTag() {
+      return tag;
+    }
+
+    public Attributes getAttributes() {
+      return attributes;
+    }
+
+    public boolean isSelfClosing() {
+      return selfClosing;
+    }
+  }
+
+  private static class LenientXMLReader implements XMLReader {
+    private ContentHandler handler = null;
+    private ErrorHandler errorHandler = null;
+    private EntityResolver entityResolver = null;
+    private DTDHandler dtdHandler = null;
+    private Set<String> features = new HashSet<>();
+    private Map<String, Object> properties = new HashMap<>();
+
+    @Override
+    public boolean getFeature(String name) throws SAXNotRecognizedException,
+        SAXNotSupportedException {
+      return features.contains(name);
+    }
+
+    @Override
+    public void setFeature(String name, boolean value)
+        throws SAXNotRecognizedException, SAXNotSupportedException {
+      if (value) {
+        features.add(name);
+      } else {
+        features.remove(name);
+      }
+    }
+
+    @Override
+    public Object getProperty(String name)
+        throws SAXNotRecognizedException, SAXNotSupportedException {
+      return properties.get(name);
+    }
+
+    @Override
+    public void setProperty(String name, Object value)
+        throws SAXNotRecognizedException, SAXNotSupportedException {
+      properties.put(name, value);
+    }
+
+    @Override
+    public void setEntityResolver(EntityResolver resolver) {
+      this.entityResolver = resolver;
+    }
+
+    @Override
+    public EntityResolver getEntityResolver() {
+      return entityResolver;
+    }
+
+    @Override
+    public void setDTDHandler(DTDHandler handler) {
+      this.dtdHandler = handler;
+    }
+
+    @Override
+    public DTDHandler getDTDHandler() {
+      return dtdHandler;
+    }
+
+    @Override
+    public void setContentHandler(ContentHandler handler) {
+      this.handler = handler;
+    }
+
+    @Override
+    public ContentHandler getContentHandler() {
+      return handler;
+    }
+
+    @Override
+    public void setErrorHandler(ErrorHandler handler) {
+      this.errorHandler = handler;
+    }
+
+    @Override
+    public ErrorHandler getErrorHandler() {
+      return errorHandler;
+    }
+
+    private enum ParserState {
+      DEFAULT,
+      INSIDE_ELEMENT,
+      INSIDE_ELEMENT_INSIDE_SINGLE_QUOTE,
+      INSIDE_ELEMENT_INSIDE_DOUBLE_QUOTE;
+    }
+
+    @Override
+    public void parse(InputSource input) throws IOException, SAXException {
+      handler.startDocument();
+
+      // We could be passed either a character stream Reader or an InputStream,
+      // so handle both.
+      BufferedReader reader;
+      if (input.getCharacterStream() != null) {
+        reader = new BufferedReader(input.getCharacterStream());
+      } else {
+        reader = new BufferedReader(new InputStreamReader(input.getByteStream(), "UTF-8"));
+      }
+
+      ParserState state = ParserState.DEFAULT;
+      int characterInt = reader.read();
+      StringBuilder currentBlock = new StringBuilder();
+      while (characterInt != -1) {
+        char c = (char) characterInt;
+        boolean append = true;
+        switch (c) {
+          case '<':
+            if (state == ParserState.DEFAULT) {
+              if (currentBlock.length() > 0) {
+                String currentBlockString = currentBlock.toString();
+                handler.characters(currentBlockString.toCharArray(), 0,
+                    currentBlockString.length());
+                currentBlock.setLength(0);
+              }
+              state = ParserState.INSIDE_ELEMENT;
+            }
+            break;
+          case '"':
+            if (state == ParserState.INSIDE_ELEMENT) {
+              state = ParserState.INSIDE_ELEMENT_INSIDE_DOUBLE_QUOTE;
+            } else if (state == ParserState.INSIDE_ELEMENT_INSIDE_DOUBLE_QUOTE) {
+              state = ParserState.INSIDE_ELEMENT;
+            }
+            break;
+          case '\'':
+            if (state == ParserState.INSIDE_ELEMENT) {
+              state = ParserState.INSIDE_ELEMENT_INSIDE_SINGLE_QUOTE;
+            } else if (state == ParserState.INSIDE_ELEMENT_INSIDE_SINGLE_QUOTE) {
+              state = ParserState.INSIDE_ELEMENT;
+            }
+            break;
+          case '>':
+            if (state == ParserState.INSIDE_ELEMENT) {
+              // Append the > now, so it's included in our call to handleElement.
+              currentBlock.append(c);
+              append = false;
+              handleElement(currentBlock.toString());
+              currentBlock.setLength(0);
+              state = ParserState.DEFAULT;
+            }
+            break;
+        }
+        if (append) {
+          currentBlock.append(c);
+        }
+        characterInt = reader.read();
+      }
+
+      handler.endDocument();
+    }
+
+    /**
+     * Takes an element of the form "<?foo?>", "<foo>", "<foo/>", or "</foo>"
+     * and calls the appropriate method on the Handler for it.
+     */
+    private void handleElement(String text) throws SAXException {
+      if (text.startsWith("<?")) {
+        if (text.endsWith("?>")) {
+          int spaceIndex = text.indexOf(' ');
+          if (spaceIndex > 0) {
+            handler.processingInstruction(
+                text.substring(2, spaceIndex), text.substring(spaceIndex + 1, text.length() - 2));
+          } else {
+            handler.processingInstruction(text.substring(2, text.length() - 2), null);
+          }
+        } else {
+          handler.processingInstruction(text.substring(2,  text.length() - 1), null);
+        }
+      } else if (text.startsWith("</")) {
+        int spaceIndex = text.indexOf(" ");
+        if (spaceIndex > 0) {
+          handler.endElement("", "", text.substring(2, spaceIndex).trim());
+        } else {
+          handler.endElement("", "", text.substring(2, text.length() - 1).trim());
+        }
+      } else {
+        LenientElementInterpreter interpreter = new LenientElementInterpreter(text);
+        handler.startElement(
+            /* uri */ "", 
+            /* localName */ "",
+            /* qName */ interpreter.getTag(),
+            /* atts */ interpreter.getAttributes());
+        if (interpreter.isSelfClosing()) {
+          handler.endElement("", "", interpreter.getTag());
+        }
+      }
+    }
+
+    @Override
+    public void parse(String systemId) throws IOException, SAXException {
+      parse(new InputSource(systemId));
+    }
+
+    private void reset() {
+      properties.clear();
+    }
+  }
+
+  protected LenientSaxParser() {
+    super();
+    this.xmlReader = new LenientXMLReader();
+  }
 
   @Override
   public Parser getParser() throws SAXException {
@@ -25,96 +366,7 @@ public class LenientSaxParser extends SAXParser {
 
   @Override
   public XMLReader getXMLReader() throws SAXException {
-    return new XMLReader() {
-
-      @Override
-      public boolean getFeature(String name) throws SAXNotRecognizedException,
-          SAXNotSupportedException {
-        // TODO Auto-generated method stub
-        return false;
-      }
-
-      @Override
-      public void setFeature(String name, boolean value)
-          throws SAXNotRecognizedException, SAXNotSupportedException {
-        // TODO Auto-generated method stub
-        
-      }
-
-      @Override
-      public Object getProperty(String name) throws SAXNotRecognizedException,
-          SAXNotSupportedException {
-        // TODO Auto-generated method stub
-        return null;
-      }
-
-      @Override
-      public void setProperty(String name, Object value)
-          throws SAXNotRecognizedException, SAXNotSupportedException {
-        // TODO Auto-generated method stub
-        
-      }
-
-      @Override
-      public void setEntityResolver(EntityResolver resolver) {
-        // TODO Auto-generated method stub
-        
-      }
-
-      @Override
-      public EntityResolver getEntityResolver() {
-        // TODO Auto-generated method stub
-        return null;
-      }
-
-      @Override
-      public void setDTDHandler(DTDHandler handler) {
-        // TODO Auto-generated method stub
-        
-      }
-
-      @Override
-      public DTDHandler getDTDHandler() {
-        // TODO Auto-generated method stub
-        return null;
-      }
-
-      @Override
-      public void setContentHandler(ContentHandler handler) {
-        // TODO Auto-generated method stub
-        
-      }
-
-      @Override
-      public ContentHandler getContentHandler() {
-        // TODO Auto-generated method stub
-        return null;
-      }
-
-      @Override
-      public void setErrorHandler(ErrorHandler handler) {
-        // TODO Auto-generated method stub
-        
-      }
-
-      @Override
-      public ErrorHandler getErrorHandler() {
-        // TODO Auto-generated method stub
-        return null;
-      }
-
-      @Override
-      public void parse(InputSource input) throws IOException, SAXException {
-        // TODO Auto-generated method stub
-        
-      }
-
-      @Override
-      public void parse(String systemId) throws IOException, SAXException {
-        // TODO Auto-generated method stub
-        
-      }
-    };
+    return xmlReader;
   }
 
   @Override
@@ -128,14 +380,24 @@ public class LenientSaxParser extends SAXParser {
   }
 
   @Override
+  public boolean isXIncludeAware() {
+    return false;
+  }
+
+  @Override
   public void setProperty(String name, Object value)
       throws SAXNotRecognizedException, SAXNotSupportedException {
-    // No properties are currently supported.
+    xmlReader.setProperty(name, value);
   }
 
   @Override
   public Object getProperty(String name) throws SAXNotRecognizedException,
       SAXNotSupportedException {
-    return null;
+    return xmlReader.getProperty(name);
+  }
+
+  @Override
+  public void reset() {
+    xmlReader.reset();
   }
 }
---
+++ tests/com/janknspank/dom/LenientSaxParserTest.java
@@ -0,0 +1,124 @@
+package com.janknspank.dom;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayInputStream;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.Attributes;
+
+import com.janknspank.dom.LenientSaxParser.LenientElementInterpreter;
+
+/**
+ * Tests for the lenient SAX parser.
+ */
+public class LenientSaxParserTest {
+  @Test
+  public void testLenientElementInterpreter() throws Exception {
+    LenientElementInterpreter interpreter = new LenientElementInterpreter("<div>");
+    assertEquals("div", interpreter.getTag());
+    assertEquals(0, interpreter.getAttributes().getLength());
+    assertFalse(interpreter.isSelfClosing());
+
+    interpreter = new LenientElementInterpreter("<span class=\"test\">");
+    assertEquals("span", interpreter.getTag());
+    assertEquals(1, interpreter.getAttributes().getLength());
+    assertEquals("test", interpreter.getAttributes().getValue("class"));
+    assertFalse(interpreter.isSelfClosing());
+
+    interpreter = new LenientElementInterpreter(
+        "<img src='image.jpg' width=500 height=600 title=\"Monster!\"/>");
+    assertEquals("img", interpreter.getTag());
+    assertEquals(4, interpreter.getAttributes().getLength());
+    assertEquals("image.jpg", interpreter.getAttributes().getValue("src"));
+    assertEquals("500", interpreter.getAttributes().getValue("width"));
+    assertEquals("600", interpreter.getAttributes().getValue("height"));
+    assertEquals("Monster!", interpreter.getAttributes().getValue("title"));
+    assertTrue(interpreter.isSelfClosing());
+
+    // This is a weird case... Let's just make sure we do something reasonable.
+    interpreter = new LenientElementInterpreter("<span clas\"/>");
+    assertEquals("span", interpreter.getTag());
+    assertEquals(1, interpreter.getAttributes().getLength());
+    assertEquals("clas", interpreter.getAttributes().getValue("clas"));
+    assertTrue(interpreter.isSelfClosing());
+  }
+
+  @Test
+  public void testLenientSaxParser() throws Exception {
+    // This would be much better as a Mockito test...
+
+    String html = "  <?xml version=\"1.0\" encoding=\"UTF-8\" ?>\r" +
+        "<html><head><title>Hello!</title></head>" +
+        "<body onload='alert(\"moose!\")'>Get <b>ready</b>..." +
+        "<img src='image.jpg' width=500 height=600 title=\"Monster!\"/>" +
+        "</body></html>";
+    LenientSaxParser parser = new LenientSaxParser();
+    final Set<String> stringsToFind = new HashSet<String>();
+    stringsToFind.add("  ");
+    stringsToFind.add("\r");
+    stringsToFind.add("Hello!");
+    stringsToFind.add("Get ");
+    stringsToFind.add("ready");
+    stringsToFind.add("...");
+
+    final Set<String> tagsToFind = new HashSet<String>();
+    tagsToFind.add("html");
+    tagsToFind.add("head");
+    tagsToFind.add("title");
+    tagsToFind.add("body");
+    tagsToFind.add("b");
+    tagsToFind.add("img");
+
+    final Set<String> endTagsToFind = new HashSet<String>();
+    endTagsToFind.add("title");
+    endTagsToFind.add("head");
+    endTagsToFind.add("b");
+    endTagsToFind.add("img");
+    endTagsToFind.add("body");
+    endTagsToFind.add("html");
+
+    parser.parse(new ByteArrayInputStream(html.getBytes()), new DefaultHandler() {
+      @Override
+      public void characters(char[] c, int start, int length) {
+        String string = String.copyValueOf(c, start, length);
+        assertTrue("Could not find characters: \"" + string + "\"", stringsToFind.contains(string));
+        stringsToFind.remove(string);
+      }
+
+      @Override
+      public void startElement(String uri, String localName, String qName,
+          Attributes attributes) {
+        assertTrue("Could not find tag: " + qName, tagsToFind.contains(qName));
+        if ("img".equals(qName)) {
+          assertEquals(4, attributes.getLength());
+          assertEquals("image.jpg", attributes.getValue("src"));
+          assertEquals("500", attributes.getValue("width"));
+          assertEquals("600", attributes.getValue("height"));
+          assertEquals("Monster!", attributes.getValue("title"));
+        }
+        tagsToFind.remove(qName);
+      }
+
+      @Override
+      public void endElement(String uri, String localName, String qName) {
+        assertTrue("Could not find end tag: " + qName, endTagsToFind.contains(qName));
+        endTagsToFind.remove(qName);
+      }
+
+      @Override
+      public void processingInstruction(String target, java.lang.String data) {
+        assertEquals("xml", target);
+      }
+    });
+
+    assertTrue(stringsToFind.isEmpty());
+    assertTrue(tagsToFind.isEmpty());
+    assertTrue(endTagsToFind.isEmpty());
+  }
+}
