372be582b75aad6fe5f809ee0e1ff95e883c982c
jonemerson <jon@jonemerson.net>
2014-12-10 22:10:19 -0800

    Add delete user servlet + business logics.

---
+++ src/com/janknspank/server/DeleteUserServlet.java
@@ -0,0 +1,51 @@
+package com.janknspank.server;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import com.janknspank.data.DataRequestException;
+import com.janknspank.data.Session;
+import com.janknspank.data.User;
+
+public class DeleteUserServlet extends NewsServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    writeSoyTemplate(resp, ".main", null);
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    // Read parameters.
+    String email = getParameter(req, "email");
+
+    // TODO(jonemerson): Add parameter validation.
+
+    // Business logic.
+    try {
+      User user = User.get(email);
+      if (user == null) {
+        resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        writeJson(resp, getErrorJson("User not found"));
+        return;
+      }
+      Session.deleteAllFromUser(user);
+      User.deleteId(user.getId());
+    } catch (DataRequestException e) {
+      resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+      writeJson(resp, getErrorJson(e.getMessage()));
+      return;
+    }
+
+    // Write response.
+    JSONObject response = new JSONObject();
+    response.put("success", true);
+    writeJson(resp, response);
+  }
+}
