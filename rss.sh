29194bc39bf933d50abe9d2f32354b31f47c10aa
jonemerson <jon@jonemerson.net>
2015-01-28 17:13:13 -0800

    Remove app engine support.  Turns out app engine won't let us talk natively to MongoDB -- and as MongoDB will provide us will absolutely amazing query support to find articles about specific industries, etc, without crazy weird schemas -- that rules it out as a deployment platform.
    Half a day wasted, sigh...

--- rss.sh
+++ rss.sh
@@ -2,7 +2,7 @@
 
 mvn package
 if [ $? -eq 0 ]; then
-  java -cp "target/NewsServer-1.0-SNAPSHOT/WEB-INF/classes:target/NewsServer-1.0-SNAPSHOT.war:target/dependency/*" \
+  java -cp target/classes:"target/NewsServer-1.0-SNAPSHOT.jar:target/dependency/*" \
       -Dfile.encoding=UTF8 \
       -Xdebug \
       -Xrunjdwp:transport=dt_socket,address=8003,server=y,suspend=n \
