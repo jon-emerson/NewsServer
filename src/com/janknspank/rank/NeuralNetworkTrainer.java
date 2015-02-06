b6709b86cc1a335a2557af74ba9508cfdd7a3adf
tomchify <tom.charytoniuk@gmail.com>
2015-02-06 12:06:43 -0800

    Clean up NeuralNetworkTrainer

--- src/com/janknspank/rank/NeuralNetworkTrainer.java
+++ src/com/janknspank/rank/NeuralNetworkTrainer.java
@@ -14,16 +14,10 @@ import org.neuroph.nnet.learning.BackPropagation;
 import org.neuroph.nnet.learning.ResilientPropagation;
 import org.neuroph.util.TransferFunctionType;
 
-import com.google.common.collect.Maps;
 import com.janknspank.ArticleCrawler;
-import com.janknspank.bizness.BiznessException;
 import com.janknspank.database.Database;
-import com.janknspank.database.DatabaseRequestException;
-import com.janknspank.database.DatabaseSchemaException;
 import com.janknspank.database.QueryOption;
-import com.janknspank.dom.parser.ParserException;
 import com.janknspank.proto.ArticleProto.Article;
-import com.janknspank.proto.UserProto.UrlRating;
 import com.janknspank.proto.UserProto.User;
 
 public class NeuralNetworkTrainer implements LearningEventListener {
@@ -32,23 +26,12 @@ public class NeuralNetworkTrainer implements LearningEventListener {
         NeuralNetworkScorer.INPUT_NODES_COUNT,
         NeuralNetworkScorer.HIDDEN_NODES_COUNT,
         NeuralNetworkScorer.OUTPUT_NODES_COUNT);
-    //neuralNetwork.setLearningRule(new MomentumBackpropagation());
 
     neuralNetwork.setLearningRule(new ResilientPropagation()); 
-    
-    // enable batch if using MomentumBackpropagation
-//    if (neuralNetwork.getLearningRule() instanceof MomentumBackpropagation) {
-//      ((MomentumBackpropagation)neuralNetwork.getLearningRule()).setBatchMode(true);
-//    }
 
- // set learning parametars
+    // set learning parameters
     LearningRule learningRule = neuralNetwork.getLearningRule();
     learningRule.addListener(this);
-
-//    MomentumBackpropagation learningRule = (MomentumBackpropagation) neuralNetwork.getLearningRule();
-//    learningRule.setLearningRate(0.7);
-//    learningRule.setMomentum(0.4);
-//    learningRule.addListener(this);
 
     System.out.println("Training neural network...");
     neuralNetwork.learn(trainingSet);
@@ -57,7 +40,7 @@ public class NeuralNetworkTrainer implements LearningEventListener {
   }
 
   private static DataSet generateTrainingDataSet(User user, Hashtable<Article, Double> ratings) {
- // Create training set.
+    // Create training set.
     DataSet trainingSet = new DataSet(
         NeuralNetworkScorer.INPUT_NODES_COUNT,
         NeuralNetworkScorer.OUTPUT_NODES_COUNT);
@@ -73,43 +56,12 @@ public class NeuralNetworkTrainer implements LearningEventListener {
 
     return trainingSet;
   }
-  
-  // Train with explicit user ratings from server.
-  private static DataSet generateTrainingDataSetFromURLRatings()
-      throws DatabaseSchemaException, ParserException, BiznessException, DatabaseRequestException {
-    Map<UrlRating, User> urlRatingToUserMap = Maps.newHashMap();
-    for (User user : Database.with(User.class).get(
-        new QueryOption.WhereNotNull("url_rating"))) {
-      for (UrlRating urlRating : user.getUrlRatingList()) {
-        urlRatingToUserMap.put(urlRating, user);
-      }
-    }
-
-    // Create training set.
-    DataSet trainingSet = new DataSet(
-        NeuralNetworkScorer.INPUT_NODES_COUNT,
-        NeuralNetworkScorer.OUTPUT_NODES_COUNT);
-    for (UrlRating rating : urlRatingToUserMap.keySet()) {
-      Article article = Database.with(Article.class).get(rating.getUrlId());
-      double[] input =
-          NeuralNetworkScorer.generateInputNodes(urlRatingToUserMap.get(rating), article);
-      double[] output = new double[]{(double)rating.getRating() / 100};
-      DataSetRow row = new DataSetRow(input, output);
-      trainingSet.addRow(row);
-    }
-
-    return trainingSet;
-  }
 
   /** Helper method for triggering a train. 
    * run ./trainneuralnet.sh to execute
    * */
   public static void main(String args[]) throws Exception {
-    // v1: Train against explicit URL ratings
-//    NeuralNetwork neuralNetwork =
-//        generateTrainedNetwork(generateTrainingDataSetFromURLRatings());
-
-    // v2: Train against Jon's Benchmarks
+    // Train against Jon's Benchmarks
     User user = Database.with(User.class).getFirst(
         new QueryOption.WhereEquals("email", "panaceaa@gmail.com"));
     Hashtable<Article, Double> ratings = new Hashtable<>();
