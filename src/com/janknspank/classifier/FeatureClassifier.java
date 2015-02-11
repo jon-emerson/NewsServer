20728f08ba3a237d070be2bf5d40d030869c168b
tomchify <tom.charytoniuk@gmail.com>
2015-02-10 17:13:17 -0800

    Added caching to FeatureClassifier

--- src/com/janknspank/classifier/FeatureClassifier.java
+++ src/com/janknspank/classifier/FeatureClassifier.java
@@ -1,11 +1,42 @@
 package com.janknspank.classifier;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.api.client.util.Maps;
 import com.janknspank.common.TopList;
 import com.janknspank.proto.ArticleProto.ArticleFeature;
 import com.janknspank.proto.ArticleProto.ArticleOrBuilder;
+import com.janknspank.proto.CoreProto.Distribution;
 import com.janknspank.rank.DistributionBuilder;
 
 public class FeatureClassifier {
+  private static FeatureClassifier instance = null;
+  // Note not all features have vectors
+  static Map<Integer, Vector> featureVectors = new HashMap<>();
+  static Map<Integer, Distribution> featureDistributions = Maps.newHashMap();
+  
+  private FeatureClassifier() {
+    for (ArticleFeatureEnum featureEnum : ArticleFeatureEnum.values()) {
+      try {
+        Feature feature = featureEnum.getFeature();
+        if (feature instanceof VectorFeature) {
+          VectorFeature vectorFeature = (VectorFeature) feature;
+          featureVectors.put(feature.getId(), vectorFeature.getVector());
+        }
+        featureDistributions.put(feature.getId(), feature.getDistribution());
+      } catch (ClassifierException e) {
+        // It's OK, just ignore this vector for now.
+      }
+    }
+  }
+
+  public static synchronized FeatureClassifier getInstance() {
+    if (instance == null) {
+      instance = new FeatureClassifier();
+    }
+    return instance;
+  }
   
   public Iterable<ArticleFeature> classify(ArticleOrBuilder article) 
       throws ClassifierException {
@@ -19,11 +50,19 @@ public class FeatureClassifier {
     return classifications;
   }
 
-  public static ArticleFeature classifyForFeature(
+  public ArticleFeature classifyForFeature(
       ArticleOrBuilder article, Feature feature) throws ClassifierException {
-    double rawSimilarity = feature.getScore(article);
+    double rawSimilarity;
+    if (feature instanceof VectorFeature) {
+      Vector articleVector = new Vector(article);
+      Vector cachedFeatureVector = featureVectors.get(feature.getId());
+      rawSimilarity = cachedFeatureVector.getCosineSimilarity(
+          UniverseVector.getInstance(), articleVector);
+    } else {
+      rawSimilarity = feature.getScore(article);
+    }
     double similarity = DistributionBuilder.projectQuantile(
-        feature.getDistribution(), rawSimilarity);
+        featureDistributions.get(feature.getId()), rawSimilarity);
     ArticleFeature classification = ArticleFeature.newBuilder()
         .setFeatureId(feature.getId())
         .setRawSimilarity(rawSimilarity)
