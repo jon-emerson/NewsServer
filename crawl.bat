3a9486b6e8a57fb7c9e09f432abcda2699dec930
jonemerson <jon@jonemerson.net>
2015-02-14 10:21:00 -0800

    Move all the .sh files into /bin/, so that we can keep making more of them without crowding our root directory.
    Remove some files we don't need anymore (Windows batch files, nbactions.xml for App Engine, parse.sh).

--- crawl.bat
+++
@@ -1,9 +0,0 @@
-:: @echo off
-:: mvn package
-:: if %errorlevel% neq 0 exit /b %errorlevel%
-java -cp target\classes:"target\NewsServer-1.0-SNAPSHOT.jar:target\dependency\*" ^
-    -Dfile.encoding=UTF8 ^
-    -Xdebug ^
-    -Xrunjdwp:transport=dt_socket,address=8009,server=y,suspend=n ^
-    -Dcom.sun.media.imageio.disableCodecLib=true ^
-    com.janknspank.ArticleCrawler
