ed8cf03c0aff54294b363080e56008e5c8662fd7
jonemerson <jon@jonemerson.net>
2015-03-16 01:09:09 -0700

    Remove auth from /viewFeed, so we can use it to debug people's streams.

--- src/com/janknspank/server/ViewFeedServlet.java
+++ src/com/janknspank/server/ViewFeedServlet.java
@@ -26,29 +26,24 @@ public class ViewFeedServlet extends StandardServlet {
   protected SoyMapData getSoyMapData(HttpServletRequest req)
       throws DatabaseSchemaException, RequestException, BiznessException {
     User user = getUser(req);
-    if (user.getEmail().equals("tom.charytoniuk@gmail.com") ||
-        user.getEmail().equals("panaceaa@gmail.com")) {
-      // Debugging for Linkedinprofile document
-//      LinkedInProfile profile = user.getLinkedInProfile();
-//      DocumentNode linkedInProfileDocument = DocumentBuilder.build(null, 
-//          new StringReader(profile.getData()));
-//      UserInterests.updateInterests(user, linkedInProfileDocument, null);
-      // Uncomment to play with profile data
+    // Debugging for Linkedinprofile document
+//    LinkedInProfile profile = user.getLinkedInProfile();
+//    DocumentNode linkedInProfileDocument = DocumentBuilder.build(null, 
+//        new StringReader(profile.getData()));
+//    UserInterests.updateInterests(user, linkedInProfileDocument, null);
+    // Uncomment to play with profile data
 
-      final TopList<Article, Double> rankedArticlesAndScores =
-          Articles.getRankedArticles(user, NeuralNetworkScorer.getInstance(), NUM_RESULTS);
-      return new SoyMapData(
-          "sessionKey", this.getSession(req).getSessionKey(),
-          "articles", ArticleSoy.toSoyListData(
-              rankedArticlesAndScores,
-              new Function<Article, Double>() {
-                @Override
-                public Double apply(Article article) {
-                  return rankedArticlesAndScores.getValue(article);
-                }
-              }));
-    } else {
-      throw new RequestException("You are not authorized to use this page.");
-    }
+    final TopList<Article, Double> rankedArticlesAndScores =
+        Articles.getRankedArticles(user, NeuralNetworkScorer.getInstance(), NUM_RESULTS);
+    return new SoyMapData(
+        "sessionKey", this.getSession(req).getSessionKey(),
+        "articles", ArticleSoy.toSoyListData(
+            rankedArticlesAndScores,
+            new Function<Article, Double>() {
+              @Override
+              public Double apply(Article article) {
+                return rankedArticlesAndScores.getValue(article);
+              }
+            }));
   }
 }
