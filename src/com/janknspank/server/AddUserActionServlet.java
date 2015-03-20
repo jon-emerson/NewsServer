db79058dac4e0e7bfb1080b41ceb2ad0ddf7ffc1
jonemerson <jon@jonemerson.net>
2015-03-20 15:49:20 -0700

    Record on_stream_for_interest on user actions, so we can differentiate engagement actions on the main stream vs. in substreams.

--- src/com/janknspank/server/AddUserActionServlet.java
+++ src/com/janknspank/server/AddUserActionServlet.java
@@ -13,6 +13,7 @@ import com.janknspank.bizness.UserInterests;
 import com.janknspank.database.Database;
 import com.janknspank.database.DatabaseRequestException;
 import com.janknspank.database.DatabaseSchemaException;
+import com.janknspank.proto.CoreProto.Entity;
 import com.janknspank.proto.UserProto.Interest;
 import com.janknspank.proto.UserProto.Interest.InterestType;
 import com.janknspank.proto.UserProto.User;
@@ -31,6 +32,14 @@ public class AddUserActionServlet extends StandardServlet {
     String readEndTimeParam = getParameter(req, "read_end_time");
     User user = getUser(req);
 
+    // If the user action happened on an interest stream (not home).
+    String interestTypeParam = getParameter(req, "on_stream_for_interest[type]");
+    String interestEntityTypeParam = getParameter(req, "on_stream_for_interest[entity][type]");
+    String interestEntityKeywordParam = getParameter(req, "on_stream_for_interest[entity][keyword]");
+    String interestEntityIdParam = getParameter(req, "on_stream_for_interest[entity][id]");
+    String interestIndustryCodeParam = getParameter(req, "on_stream_for_interest[industry_code]");
+    String interestIntentCodeParam = getParameter(req, "on_stream_for_interest[intent_code]");
+
     // parameter validation
     UserAction.ActionType actionType = UserAction.ActionType.valueOf(actionTypeParam);
     if (actionType == null) {
@@ -44,7 +53,7 @@ public class AddUserActionServlet extends StandardServlet {
         .setUrl(urlParam)
         .setUrlId(urlIdParam)
         .setCreateTime(System.currentTimeMillis());
-    
+
     // Save user model components to the user action. If they change later
     // then we won't lose the context for the neural network
     // Note: getInterests returns only non-tombstoned interests
@@ -56,6 +65,29 @@ public class AddUserActionServlet extends StandardServlet {
       } else if (userInterest.getType() == InterestType.LINKED_IN_CONTACTS) {
         actionBuilder.addAllLinkedInContact(user.getLinkedInContactList());
       }
+    }
+
+    // If the user was viewing a different stream than home, record everything
+    // we know about it.  Let's not bother validating anything here: Let's grab
+    // what we can, and remember everything we can.  We can do validation later.
+    if (interestTypeParam != null) {
+      Interest.Builder interestBuilder = Interest.newBuilder()
+          .setType(InterestType.valueOf(interestTypeParam));
+      if (interestEntityKeywordParam != null
+          && interestEntityIdParam != null
+          && interestEntityTypeParam != null) {
+        interestBuilder.setEntity(Entity.newBuilder()
+            .setKeyword(interestEntityKeywordParam)
+            .setId(interestEntityIdParam)
+            .setType(interestEntityTypeParam));
+      }
+      if (interestIndustryCodeParam != null) {
+        interestBuilder.setIndustryCode(Integer.parseInt(interestIndustryCodeParam));
+      }
+      if (interestIntentCodeParam != null) {
+        interestBuilder.setIntentCode(interestIntentCodeParam);
+      }
+      actionBuilder.setOnStreamForInterest(interestBuilder);
     }
 
     if (actionType == UserAction.ActionType.READ_ARTICLE) {
