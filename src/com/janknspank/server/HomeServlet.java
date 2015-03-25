dbe77550204878595a673cedb592d1fc399b3068
tomchify <tom.charytoniuk@gmail.com>
2015-03-25 13:31:00 -0700

    Bits of the desktop site + Terms, Privacy & Copyright policies.

---
+++ src/com/janknspank/server/HomeServlet.java
@@ -0,0 +1,21 @@
+package com.janknspank.server;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.google.template.soy.data.SoyMapData;
+import com.janknspank.database.DatabaseSchemaException;
+
+@ServletMapping(urlPattern = "/secret")
+public class HomeServlet extends StandardServlet {
+  /**
+   * Returns any Soy data necessary for rendering the .main template for this
+   * servlet's Soy page.
+   */
+  @Override
+  protected SoyMapData getSoyMapData(HttpServletRequest req)
+      throws DatabaseSchemaException, RequestException {
+    return new SoyMapData(
+        "title", "Spotter - News for Your Career App",
+        "tab", 0);
+  }
+}
