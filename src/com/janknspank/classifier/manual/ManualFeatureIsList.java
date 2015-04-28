8708209a1711f4feb669b3101fec5f31f38d32fd
tomchify <tom.charytoniuk@gmail.com>
2015-04-28 10:48:48 -0700

    IsList cleanup and some sites.

--- src/com/janknspank/classifier/manual/ManualFeatureIsList.java
+++ src/com/janknspank/classifier/manual/ManualFeatureIsList.java
@@ -11,25 +11,19 @@ import com.janknspank.classifier.ManualHeuristicFeature;
 import com.janknspank.proto.ArticleProto.ArticleOrBuilder;
 
 public class ManualFeatureIsList extends ManualHeuristicFeature {
-  // Acquisition keywords.
-  // NOTE: Do not use the word "Acquisition".  It matches articles like this:
-  // http://www.channelnewsasia.com/news/singapore/parliament-passes-changes/1714406.html
   private static final Map<Pattern, Double> TITLE_SCORES =
       ImmutableMap.<Pattern, Double>builder()
-          .put(Pattern.compile("[0-9]{1,2} ways"), 1.0)
-          .put(Pattern.compile("[0-9]{1,2} tips"), 1.0)
-          .put(Pattern.compile("[0-9]{1,2} things"), 1.0)
-          .put(Pattern.compile("[0-9]{1,2} numbers"), 1.0)
-          .put(Pattern.compile("[0-9]{1,2} reasons"), 1.0)
-          .put(Pattern.compile("^(two|three|four|five|six|seven|eight|nine|ten|eleven|twelve) "), 0.9)
-          .put(Pattern.compile("^[0-9]{1,2} "), 0.9)
-          .put(Pattern.compile("this week:"), 0.9)
+          .put(Pattern.compile("[0-9]{1,2}.* (awesome|photos|apps|that|ways|tips|things|numbers|reasons|strategies|you|should)"), 1.0)
+          .put(Pattern.compile("^(two|three|four|five|six|seven|eight|nine|ten|eleven|twelve) .* (awesome|photos|apps|that|ways|tips|things|numbers|reasons|strategies|you|should)"), 0.9)
+          .put(Pattern.compile(" this week:"), 0.9)
           .build();
   private static final Iterable<Pattern> TITLE_BLACKLIST =
-      Arrays.asList();
+      Arrays.asList(Pattern.compile("[0-9]{1,2} in [0-9]{1,2}"),
+          Pattern.compile("\\/[0-9][0-9] numbers")
+          );
   private static final Map<Pattern, Double> BODY_SCORES =
       ImmutableMap.<Pattern, Double>builder()
-          .put(Pattern.compile("the (two|three|four|five|six|seven|eight|nine|ten|eleven|twelve) best .* of the week"), 0.9)
+          .put(Pattern.compile("the (three|four|five|six|seven|eight|nine|ten|eleven|twelve) best .* of the week"), 0.9)
           .put(Pattern.compile("the [0-9]+ best .* of the week"), 0.9)
           .build();
   private static final Iterable<Pattern> BODY_BLACKLIST = Arrays.asList();
