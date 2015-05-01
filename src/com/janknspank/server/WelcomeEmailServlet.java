b2ab98a264264a95b04c5e06ee216305e173a829
tomchify <tom.charytoniuk@gmail.com>
2015-05-01 14:09:18 -0700

    Beginning of welcome email + industry bug fixes

---
+++ src/com/janknspank/server/WelcomeEmailServlet.java
@@ -0,0 +1,21 @@
+package com.janknspank.server;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.google.template.soy.data.SoyMapData;
+import com.janknspank.database.DatabaseSchemaException;
+
+@ServletMapping(urlPattern = "/welcome_email")
+public class WelcomeEmailServlet extends StandardServlet {
+  /**
+   * Returns any Soy data necessary for rendering the .main template for this
+   * servlet's Soy page.
+   */
+  @Override
+  protected SoyMapData getSoyMapData(HttpServletRequest req)
+      throws DatabaseSchemaException, RequestException {
+    return new SoyMapData(
+        "title", "Spotter - Business news, personalized",
+        "tab", 0);
+  }
+}
