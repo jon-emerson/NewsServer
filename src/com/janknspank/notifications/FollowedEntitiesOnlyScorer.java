0de14786aae7b8e92c94f3cb79cff48cb5d9355a
jonemerson <jon@jonemerson.net>
2015-06-02 00:31:28 -0700

    Fixes.

--- src/com/janknspank/notifications/FollowedEntitiesOnlyScorer.java
+++ src/com/janknspank/notifications/FollowedEntitiesOnlyScorer.java
@@ -2,14 +2,18 @@ package com.janknspank.notifications;
 
 import java.util.List;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 import com.janknspank.bizness.Articles;
 import com.janknspank.bizness.BiznessException;
+import com.janknspank.bizness.TimeRankingStrategy.MainStreamStrategy;
 import com.janknspank.database.DatabaseSchemaException;
 import com.janknspank.proto.ArticleProto.Article;
 import com.janknspank.proto.UserProto.Interest;
 import com.janknspank.proto.UserProto.User;
 import com.janknspank.proto.UserProto.Interest.InterestType;
+import com.janknspank.rank.DiversificationPass;
+import com.janknspank.rank.NeuralNetworkScorer;
 
 public class FollowedEntitiesOnlyScorer extends BlendScorer {
   @Override
@@ -20,9 +24,15 @@ public class FollowedEntitiesOnlyScorer extends BlendScorer {
         entityInterests.add(interest);
       }
     }
-    return Articles.getMainStream(user.toBuilder()
-        .clearInterest()
-        .addAllInterest(entityInterests)
-        .build());
+    return Articles.getRankedArticles(
+        user.toBuilder()
+            .clearInterest()
+            .addAllInterest(entityInterests)
+            .build(),
+        NeuralNetworkScorer.getInstance(),
+        new MainStreamStrategy(),
+        new DiversificationPass.MainStreamPass(),
+        25 /* results */,
+        ImmutableSet.<String>of());
   }
 }
